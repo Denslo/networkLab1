49fcb7686c41af58f07a154efd88f86ec3096ecc
shai <shaiambar@yahoo.com>
2013-11-24 17:14:02 +0200

    add respons

--- src/Response.java
+++ src/Response.java
@@ -1,4 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Response {
+	
+	private Map<String, String> respone = null;
+	
+	public Response() {
+		respone = new HashMap<String, String>();
+		
+	}
+	
+	public String getHeaderValue(String key){
+		
+	}
+	
+	public void add(String key, String value){
+		
+	}
+	
+	public String getResponsType(){
+		
+	}
+	
+	public void addResponsType(String value){
+		
+	}
 
 }
